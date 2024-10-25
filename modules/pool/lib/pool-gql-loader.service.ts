8c27fb8abe47e134c511b11fbdb5e95fdf80ad0f
franzns <93920061+franzns@users.noreply.github.com>
2024-10-25 17:00:07 +0200

    fix total apr (#1112)

--- modules/pool/lib/pool-gql-loader.service.ts
+++ modules/pool/lib/pool-gql-loader.service.ts
@@ -1199,7 +1199,20 @@ export class PoolGqlLoaderService {
                                 apr: { __typename: 'GqlPoolAprTotal', total: `${item.apr}` },
                             }),
                         );
-                        const apr = _.sumBy(items, 'apr');
+                        let apr = 0;
+                        for (const item of items) {
+                            if (
+                                item.type === 'SWAP_FEE_24H' ||
+                                item.type === 'SWAP_FEE_7D' ||
+                                item.type === 'SWAP_FEE_30D' ||
+                                item.type === 'SURPLUS_24H' ||
+                                item.type === 'SURPLUS_7D' ||
+                                item.type === 'SURPLUS_30D'
+                            ) {
+                            } else {
+                                apr += item.apr;
+                            }
+                        }
                         const title = `${group.charAt(0) + group.slice(1).toLowerCase()} boosted APR`;
 
                         return {
