58d3e2506cf4203e06310eb20f81a49fc0274879
Bruno <bruno@balancerlabs.dev>
2025-01-14 03:35:34 -0300

    SOR - Fix split paths to support larger swaps (#1460)
    * SOR - Fix split paths to support larger swaps
    
    * Remove unnecessary debug log

---
+++ .changeset/light-dancers-push.md
@@ -0,0 +1,5 @@
+---
+'backend': patch
+---
+
+SOR - Fix split paths to support larger swaps
--- modules/sor/sor-debug.test.ts
+++ modules/sor/sor-debug.test.ts
@@ -7,25 +7,26 @@ import { TokenController } from '../controllers/token-controller';
 import { sorService } from './sor.service';
 
 describe('sor debugging', () => {
-    it('sor v2 arb eth->usdc', async () => {
-        const chain = Chain.FANTOM;
+    it('sor v2', async () => {
+        const useProtocolVersion = 2;
+        const chain = Chain.ARBITRUM;
 
         const chainId = Object.keys(chainIdToChain).find((key) => chainIdToChain[key] === chain) as string;
         initRequestScopedContext();
         setRequestScopedContextValue('chainId', chainId);
         //only do once before starting to debug
-        // await poolService.syncAllPoolsFromSubgraph();
-        // await poolService.loadOnChainDataForAllPools();
-        // await poolService.updateLiquidityValuesForPools();
+        // await PoolController().addPoolsV2(chain);
+        // await PoolController().syncOnchainDataForAllPoolsV2(chain);
+        // await PoolController().syncChangedPoolsV2(chain);
+        // await PoolController().updateLiquidityValuesForActivePools(chain);
 
         const swaps = await sorService.getSorSwapPaths({
             chain,
-            tokenIn: '0xf24bcf4d1e507740041c9cfd2dddb29585adce1e', // BAL
-            tokenOut: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83', // WETH
+            tokenIn: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', // weth
+            tokenOut: '0x5979d7b546e38e414f7e9822514be443a4800529', // wsteth
             swapType: 'EXACT_IN',
-            swapAmount: '1',
-            queryBatchSwap: false,
-            // useProtocolVersion: 2,
+            swapAmount: '250',
+            useProtocolVersion,
             // callDataInput: {
             //     receiver: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
             //     sender: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
@@ -37,25 +38,30 @@ describe('sor debugging', () => {
         expect(parseFloat(swaps.returnAmount)).toBeGreaterThan(0);
     }, 5000000);
 
-    it.only('sor v3 mainnet wstETH -> waGnowstETH', async () => {
-        const chain = Chain.GNOSIS;
+    it('sor v3', async () => {
+        const useProtocolVersion = 3;
+        const chain = Chain.MAINNET;
 
         const chainId = Object.keys(chainIdToChain).find((key) => chainIdToChain[key] === chain) as string;
         initRequestScopedContext();
         setRequestScopedContextValue('chainId', chainId);
-        //only do once before starting to debug
+        // only do once before starting to debug
         await PoolController().reloadPoolsV3(chain);
         await TokenController().syncErc4626Tokens(chain);
         await TokenController().syncErc4626UnwrapRates(chain);
 
+        // to update liquidity values, first update the token prices through a mutation
+        // yarn dev; yarn mutation 'tokenReloadTokenPrices(chains: [MAINNET])' 1
+        // await PoolController().updateLiquidityValuesForActivePools(chain);
+
         const swaps = await sorService.getSorSwapPaths({
             chain,
-            tokenIn: '0x773cda0cade2a3d86e6d4e30699d40bb95174ff2', // wagnowsteth
-            tokenOut: '0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6', // wsteth
+            tokenIn: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
+            tokenOut: '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
             swapType: 'EXACT_IN',
-            swapAmount: '1',
-            useProtocolVersion: 3,
-            poolIds: ['0x272d6be442e30d7c87390edeb9b96f1e84cecd8d'], // boosted
+            swapAmount: '100',
+            useProtocolVersion,
+            // poolIds: ['0x10a04efba5b880e169920fd4348527c64fb29d4d'], // boosted
         });
 
         console.log(swaps.returnAmount);
@@ -65,65 +71,5 @@ describe('sor debugging', () => {
             }
         }
         expect(parseFloat(swaps.returnAmount)).toBeGreaterThan(0);
-    }, 5000000);
-
-    it('sor v2 fantom gyro', async () => {
-        const chain = Chain.FANTOM;
-
-        const chainId = Object.keys(chainIdToChain).find((key) => chainIdToChain[key] === chain) as string;
-        initRequestScopedContext();
-        setRequestScopedContextValue('chainId', chainId);
-        //only do once before starting to debug
-        // await PoolController().syncOnchainDataForPoolsV2(chain, [
-        //     '0xff236989201b0f2691c3208af68d15c6d79ce8a7000200000000000000000903',
-        // ]);
-        // await PoolController().updateLiquidityValuesForActivePools(chain);
-
-        const SFTMX = '0xd7028092c830b5c8fce061af2e593413ebbc1fc1';
-        const WFTM = '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83';
-        const USDCE = '0x2f733095b80a04b38b0d10cc884524a3d09b836a';
-
-        // const swapPaths = await sorService.getSorSwapPaths({
-        //     chain,
-        //     tokenIn: WFTM,
-        //     tokenOut: USDCE,
-        //     swapType: 'EXACT_IN',
-        //     swapAmount: '10000',
-        //     queryBatchSwap: false,
-        //     useProtocolVersion: 2,
-        //     // callDataInput: {
-        //     //     receiver: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
-        //     //     sender: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
-        //     //     slippagePercentage: '0.1',
-        //     // },
-        // });
-
-        const swaps = await sorService.getSorSwaps({
-            chain,
-            tokenIn: WFTM,
-            tokenOut: USDCE,
-            swapType: 'EXACT_IN',
-            swapAmount: '10000',
-            // queryBatchSwap: false,
-            // useProtocolVersion: 3,
-            // callDataInput: {
-            //     receiver: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
-            //     sender: '0xb5e6b895734409Df411a052195eb4EE7e40d8696',
-            //     slippagePercentage: '0.1',
-            // },
-            swapOptions: {
-                maxPools: 8,
-            },
-        });
-
-        for (const route of swaps.routes) {
-            for (const hop of route.hops) {
-                if (hop.pool.id === '0xff236989201b0f2691c3208af68d15c6d79ce8a7000200000000000000000903') {
-                    console.log(`ECLP`);
-                }
-            }
-        }
-        expect(parseFloat(swaps.returnAmount)).toBeGreaterThan(0);
-        console.log(swaps.returnAmount);
     }, 5000000);
 });
